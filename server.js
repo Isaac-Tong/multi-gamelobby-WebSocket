208f4a860d401dee9d32b07a912c2ff658457060
Isaac Tong <isaact943@outlook.com>
2020-07-04 06:57:52 +0800

    Lobby page backend completed. When connect and disconnect, socket emits to all connected clients in the room

--- server.js
+++ server.js
@@ -47,25 +47,24 @@ const io = socket(server);
 io.on('connection', (socket)=>{
     
     
-
     //Check if connection has been established
-    console.log('Current socket connection:', socket.id)
+    // console.log('Current socket connection:', socket.id)
 
     //Connect to a room
     socket.on('Join_Game', async (data) => {
         
         
-        //Subscribe to the room defined by data.roomID
-        socket.join(data.roomID);
-
+       
+        
         //Check if roomID is stored in activeGame database
         const query = await activeGameModel.findOne({roomID: data.roomID});
-
         //Append username and socketID
         const socketUsername = data.username + '.' + socket.id;
-
+        
         //Add user and socketID to the roomID document if there is already an active document
         if(query){
+    
+            
             await activeGameModel.findOneAndUpdate({roomID: data.roomID}, { '$push' : {username: socketUsername}}, {useFindAndModify: false});
             
             const newSocket = new socketModel({
@@ -73,46 +72,93 @@ io.on('connection', (socket)=>{
                 username: data.username,
                 roomID: data.roomID
             });
-
             await newSocket.save();
+
+            //Subscribe to the room defined by data.roomID
+            socket.join(data.roomID);
+
+            const connected = await activeGameModel.findOne({roomID: data.roomID});
+
+            const connectedUsers = {
+                connected: [],
+            };
+
+            for(let i = 0; i < connected.username.length; i++){
+                //Split the string
+                const arr = connected.username[i].split('.');
+    
+                connectedUsers.connected[i] = arr[0];
+            }
+
+            io.to(data.roomID).emit('Update_User', connectedUsers)
+
             return;
         }
         
-
         //Create a new active document if none has been created yet
         const roomData = {
             roomID: data.roomID,
             username: []
         }
-        
         roomData.username.push(socketUsername);
         const newGame = new activeGameModel(roomData);
         newGame.save();
-
+        
         const newSocket = new socketModel({
             socket: socket.id,
             username: data.username,
             roomID: data.roomID
         
         });
-
         await newSocket.save();
+
+        //Subscribe to the room defined by data.roomID
+        socket.join(data.roomID);
+
+        const connected = await activeGameModel.findOne({roomID: data.roomID});
+
+        const connectedUsers = {
+            connected: [],
+        };
+
+        for(let i = 0; i < connected.username.length; i++){
+            //Split the string
+            const arr = connected.username[i].split('.');
+
+            connectedUsers.connected[i] = arr[0];
+        }
+
+        io.to(data.roomID).emit('Update_User', connectedUsers)
+
     })
 
     socket.on('disconnect', async () => {
+        console.log('disonnected');
+        
         //Find which roomID and username the socket is referring to
         const removedSocket = await socketModel.findOneAndRemove({socket: socket.id}, {useFindAndModify: false});
         
         //Delete the corresponding socket from the activeGames database
         const appendedSocketUsername = removedSocket.username +  '.' + removedSocket.socket;
         await activeGameModel.update({roomID: removedSocket.roomID}, { "$pull": {username : appendedSocketUsername} })
-        
-        
 
 
-        
+        //EMIT TO ALL SOCKETS THAT ONE USER HAS LEFT
+        const connected = await activeGameModel.findOne({roomID: removedSocket.roomID});
 
-        
+        const connectedUsers = {
+            connected: [],
+        };
+
+        for(let i = 0; i < connected.username.length; i++){
+            //Split the string
+            const arr = connected.username[i].split('.');
+
+            connectedUsers.connected[i] = arr[0];
+        }
+
+        io.to(removedSocket.roomID).emit('Update_User', connectedUsers)
+
     })
 
     
